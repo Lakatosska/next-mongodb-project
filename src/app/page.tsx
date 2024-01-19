0d09e85c73bd862254a336cf4553afa6ca6f5052
Elena Nikiforova <vizualnoe@gmail.com>
2024-01-19 10:16:38 +0100

    add state and conditional rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+"use client"
+
 import Image from 'next/image'
 import styles from './page.module.css'
+import { useState } from "react"
+
+type Todo = {
+  _id: string
+  text: string | null
+  completed: boolean
+}
 
 export default function Home() {
+  const [isLoading, setLoading] = useState(true)
+  const [todos, setTodos] = useState<Todo | null>(null)
+  const [newTodoText, setNewTodoText] = useState<string>("")
+  const [editTodo, setEditTodo] = useState<Todo | null>(null)
+
   return (
     <main className="font-mulish grid lg:place-items-start place-items-center w-full bg-black text-purple-500 min-h-screen">
       <div className="flex lg:flex-row flex-col gap-5 lg:justify-start justify-center lg:items-start items-center w-full">
@@ -10,34 +24,32 @@ export default function Home() {
             Todo list
           </h1>
           {/* edit todo */}
-          <>
-            <input 
-              className="w-full lg:w-8/12 bg-black border border-yellow-400 py-4 text-xl rounded-lg text-purple-400 outline-none px-3"
-              type="text"
-            />
-            <button className="bg-slate-800 px-6 py-2 rounded-lg my-7 text-green-400 text-lg uppercase font-semibold">
-              Save
-            </button>
-          </>
-          {/* add todo */}
-          <>
-            <input 
-              className="w-full lg:w-8/12 bg-black border border-purple-400 py-4 text-xl rounded-lg text-purple-400 outline-none px-3"
-              type="text"
-              placeholder="Write here..."
-            />
-            <button className="bg-slate-800 px-6 py-2 rounded-lg my-7 text-green-400 text-lg uppercase font-semibold">
-              Add Todo
-            </button>
-          </>
-
-          
-
-
+          {editTodo ? (
+            <>
+              <input 
+                className="w-full lg:w-8/12 bg-black border border-yellow-400 py-4 text-xl rounded-lg text-purple-400 outline-none px-3"
+                type="text"
+              />
+              <button className="bg-slate-800 px-6 py-2 rounded-lg my-7 text-green-400 text-lg uppercase font-semibold">
+                Save
+              </button>
+            </>
+            ) : (
+            /* add todo */
+              <>
+                <input 
+                  className="w-full lg:w-8/12 bg-black border border-purple-400 py-4 text-xl rounded-lg text-purple-400 outline-none px-3"
+                  type="text"
+                  placeholder="Write here..."
+                />
+                <button className="bg-slate-800 px-6 py-2 rounded-lg my-7 text-green-400 text-lg uppercase font-semibold">
+                  Add Todo
+                </button>
+              </>
+            )
+          }   
 
         </div>
-
-        
 
       </div>
       
