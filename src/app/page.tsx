a457cadad26527df7fc4215fa1de1041a759d51f
Elena Nikiforova <vizualnoe@gmail.com>
2024-01-23 00:22:39 +0100

    fix layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 "use client"
 
-import Image from 'next/image'
-import styles from './page.module.css'
-import { useState, useEffect, SetStateAction } from "react"
+import { useState, useEffect } from "react"
 
 type Todo = {
   _id: string
@@ -14,13 +12,10 @@ type Todo = {
 export default function Home() {
   const [isLoading, setLoading] = useState(true)
   const [todos, setTodos] = useState<Todo[]>([])
-  const [newTodoText, setNewTodoText] = useState<string>("")
-  const [editTodo, setEditTodo] = useState<Todo | null>(null)
   const [title, setTitle] = useState<string>("")
   const [text, setText] = useState<string>("")
+  const [editTodo, setEditTodo] = useState<Todo | null>(null)
 
-  const onTitleChanged = (e : any) => setTitle(e.target.value)
-  const onTextChanged = (e : any) => setText(e.target.value)
 
   useEffect(() => {
     fetch("http://localhost:3000/api/todo")
@@ -30,6 +25,9 @@ export default function Home() {
       setLoading(false)
     })
   }, [])
+
+  const onTitleChanged = (e : any) => setTitle(e.target.value)
+  const onTextChanged = (e : any) => setText(e.target.value)
 
   const handleSubmitAddTodo = async(event : any) => {
     event.preventDefault();
@@ -107,24 +105,6 @@ export default function Home() {
     }
   }
 
-  // const toggleTodo = async (id: string, completed: boolean) => {
-  //   const response = await fetch('http://localhost:3000/api/todo', {
-  //     method: 'PUT',
-  //     body: JSON.stringify({ id, completed: !completed }),
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     }
-  //   })
-
-  //   if (response.status === 200) {
-  //     setTodos(
-  //       todos.map((todo: Todo) =>
-  //         todo._id === id ? { ...todo, completed: !completed } : todo
-  //       )
-  //     )
-  //   }
-  // }
-
   const toggleTodo = async (id: string, title: string, text: string, completed : boolean) => {
     const response = await fetch('http://localhost:3000/api/todo', {
       method: 'PUT',
@@ -154,31 +134,36 @@ export default function Home() {
 
 
   return (
-    <main className="font-mulish grid lg:place-items-start place-items-center w-full bg-black text-purple-500 min-h-screen">
+    <main className="font-mulish grid lg:place-items-start place-items-center w-full bg-stone-50 text-purple-500 min-h-screen">
       <div className="flex lg:flex-row flex-col gap-5 lg:justify-start justify-center lg:items-start items-center w-full">
-        <div className="sm:w-9/12 lg:w-6/12 w-full px-4 lg:my-10 flex flex-col justify-center items-center">
-          <h1 className="text-4xl py-8 lg:py-0 lg:pt-4 lg:pb-14 text-yellow-500">
-            Todo list
+        <div className="sm:w-12/12 lg:w-12/12 w-full px-4 lg:my-10 flex flex-col justify-center items-center">
+          <h1 className="text-4xl py-8 lg:py-0 lg:pt-4 lg:pb-14 text-cyan-800 font-bold">
+            My Todo List
           </h1>
           {/* edit todo */}
           {editTodo ? (
             <>
-              <form onSubmit = {handleSubmitEditTodo}>
+              <form 
+                onSubmit = {handleSubmitEditTodo}
+                className="w-full"
+                style={{ width: "400px", display: "flex", flexDirection: "column" }}
+
+              >
                 <input
-                  className="w-full lg:w-8/12 bg-black border border-yellow-400 py-4 text-xl rounded-lg text-purple-400 outline-none px-3"
+                  className="w-full lg:w-12/12 py-4 text-xl rounded-lg text-purple-400 outline-none px-3"
                   type="text"
                   name="title"
                   value={editTodo.title!}
                   onChange={(e) => setEditTodo({...editTodo, title: e.target.value})}
                 />
                 <textarea
-                  className="w-full lg:w-8/12 bg-black border border-yellow-400 py-4 text-xl rounded-lg text-purple-400 outline-none px-3"
+                  className="w-full bg-black border border-yellow-400 py-4 text-xl rounded-lg text-purple-400 outline-none px-3"
                   name="text"
                   value={editTodo.text!}
                   onChange={(e) => setEditTodo({...editTodo, text: e.target.value})}
                 />
               
-                <button className="bg-slate-800 px-6 py-2 rounded-lg my-7 text-green-400 text-lg uppercase font-semibold">
+                <button className="bg-slate-400 px-6 py-2 rounded-lg my-7 text-green-400 text-lg uppercase font-semibold">
                   Save
                 </button>
               </form>
@@ -188,7 +173,7 @@ export default function Home() {
               <>
                 <form onSubmit={handleSubmitAddTodo} style={{ width: "400px", display: "flex", flexDirection: "column" }}>
                   <input 
-                    className="w-full lg:w-8/12 bg-black border border-purple-400 py-4 text-xl rounded-lg text-purple-400 outline-none px-3"
+                    className="w-full lg:w-12/12 bg-teal-100 border border-purple-400 py-4 text-xl rounded-lg text-purple-400 outline-none px-3"
                     type="text"
                     name="title"
                     placeholder="Write here your title..."
@@ -197,7 +182,7 @@ export default function Home() {
                   />
                   
                   <textarea
-                    className="w-full lg:w-8/12 bg-black border border-purple-400 py-4 text-xl rounded-lg text-purple-400 outline-none px-3"
+                    className="w-full lg:w-12/12 bg-teal-100 border border-purple-400 py-4 text-xl rounded-lg text-purple-400 outline-none px-3"
                     name="text"
                     placeholder="Write here your text..."
                     value={text!}
@@ -205,7 +190,7 @@ export default function Home() {
                   />
                 
                   <button 
-                    className="bg-slate-800 px-6 py-2 rounded-lg my-7 text-green-400 text-lg uppercase font-semibold"
+                    className="bg-slate-300 px-6 py-2 rounded-lg my-7 text-cyan-800 text-lg uppercase font-semibold"
                   >
                     Add Todo
                   </button>
@@ -229,26 +214,29 @@ export default function Home() {
               {!isLoading && todos && todos.map((todo: Todo) => (
                 <li 
                   key={todo._id}
-                  className="bg-slate-900 px-6 py-5 rounded-lg my-3 hover:text-green-400 text-lg w-full flex justify-between items-start"
+                  className="bg-teal-200 px-6 py-5 rounded-lg my-3 hover:text-green-400 text-lg w-full flex justify-between items-start"
                 >
-                  <div className="flex justify-start items-start w-8/12">
+                  <div className="flex justify-start items-start w-8/12 text-wrap">
                     <input
                       type="checkbox"
                       className="w-5 h-5 cursor-pointer mt-1"
                       checked={todo.completed}
                       onChange={() => toggleTodo(todo._id, todo.title!, todo.text!, todo.completed)}
                     />
-                    <div style={{ width: "400px", display: "flex", flexDirection: "column" }}>
+                    <div 
+                      className="text-wrap"
+                      style={{ width: "400px", display: "flex", flexDirection: "column",  }}
+                    >
                       <span 
                         className={`${
                           todo.completed ? "line-through" : "list-none"
-                        } px-4 w-full text-yellow-500`}>
+                        } px-4 w-full text-cyan-800 font-semibold`}>
                         {todo.title}
                       </span>
                       <span 
                         className={`${
                           todo.completed ? "line-through" : "list-none"
-                        } px-4 w-full text-yellow-500`}>
+                        } px-4 w-full text-cyan-800 text-wrap`}>
                         {todo.text}
                       </span>
                     </div>
